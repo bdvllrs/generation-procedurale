529e609dbae0e9a0490d70759f1575a05b621c3f
redstorm <pierre.cadart@gmail.com>
2017-05-27 12:40:15 +0200

    Added Show.py

---
+++ Show.py
@@ -0,0 +1,52 @@
+import pygame
+import random
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+def show(the_map):
+    pygame.init()
+    pygame.display.set_mode((600,600), DOUBLEBUF | OPENGL)
+
+    glEnable(GL_DEPTH_TEST)
+
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(45, 1, 0.1, 50.0)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    angle = 0
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        glClearColor(0, 0, 0, 0)
+        
+        glPushMatrix()
+        glTranslatef(0.0,0.0, -5)
+        glRotated(30, 1, 0, 0)
+        glRotated(angle, 0, 1, 0)
+        
+        glTranslatef(-0.5, 0, -0.5)
+        glScalef(1/the_map.side, 1, 1/the_map.side)
+        glColor3f(0.1, 0.9, 0.1)
+        glBegin(GL_QUADS)
+        for i in range(the_map.side-1):
+            for j in range(the_map.side-1):
+                glVertex3f(i  , the_map.map[i][j]    , j)
+                glVertex3f(i+1, the_map.map[i+1][j]  , j)
+                glVertex3f(i+1, the_map.map[i+1][j+1], j+1)
+                glVertex3f(i  , the_map.map[i][j+1]  , j+1)
+        glEnd()
+        
+        glPopMatrix()
+        
+        pygame.display.flip()
+        angle += .5
+        pygame.time.wait(10)
+    pygame.quit()
+
