f317a2dbfd9f605cb03f5d98ab75cce0c512f26a
redstorm45 <pierre.cadart@gmail.com>
2017-05-30 01:25:27 +0200

    Camera movements, lighting and transparent water

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from Fractal import PerlinNoise
 
 
 if __name__ == '__main__':
-    m = Map(5)
-    m.generate_map(seed=1)
+    m = Map(257)
+    m.generate_map(seed=2)
     # print(m.map)
     # plt.imshow(m.map, cmap=plt.cm.binary)
     # plt.show()
